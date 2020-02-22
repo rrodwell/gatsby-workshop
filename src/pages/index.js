eb5ba67099bd715460cd1298f265f1b4cb8ca5ea
Ryan Rodwell <rrodwell@deloitte.com>
2020-02-22 14:19:43 -0500

    creating a template component & gql query which will be used to build a common page for every blog post

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,24 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import PostList from "../components/postList"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const posts = data.allMarkdownRemark.edges
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <PostList posts={posts} />
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 //* Queries that are not being exported will not be ran a build time
 //* Gatsby finds all queries that being exported, fetches the data, and renders the pages a build
+//* Defining queries in a component like this will be passed as a data prop to the component
 export const QUERY = graphql`
   query ALL_POST_QUERY {
     allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
---
+++ src/templates/blogPosts.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+const BlogPosts = ({ data }) => {
+  const { html: __html } = data.markdownRemark;
+  const { title, date, author, tag } = data.markdownRemark.frontmatter;
+  return (
+    <Layout>
+      <h1>{title}</h1>
+      <h4>{date}</h4>
+      <p>Tags: {tag}</p>
+      <div
+        dangerouslySetInnerHTML={{ __html }}
+      />
+      <p>written by {author}</p>
+    </Layout>
+  );
+}
+
+//* markdownRemark is a query made available from the gatsby-transformer-remark package
+export const POST_BY_PATH = graphql`
+  query POST_BY_PATH($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      id
+      html
+      frontmatter {
+        path
+        title
+        author
+        date
+        tag
+      }
+    }
+  }
+`;
+
+export default BlogPosts
